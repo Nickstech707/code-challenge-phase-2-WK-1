a94cc64ea7f44c5f86f42e706f3b6af8f2c99e46
Nicholas Muriithi <nickstech707@gmail.com>
2024-04-28 17:07:10 +0300

    Added search function and updated the CSS file

--- bank-of-flatiron/src/App.js
+++ bank-of-flatiron/src/App.js
@@ -1,68 +1,13 @@
 import "./App.css";
-import React, { useState } from "react";
+import React from "react";
 import Header from "./components/Header";
 import SearchBox from "./components/Search";
-import AddTransactions from "./components/AddTransactions";
-import TableDataList from "./components/TableData";
+
 function App() {
-  const [transactions, setTransactions] = useState([
-    {
-      date: "2019-12-01",
-      description: "Paycheck from Bob's Burgers",
-      category: "Income",
-      amount: 1000,
-    },
-    {
-      date: "2019-12-01",
-      description: "South by Southwest Quinoa Bowl at Fresh & Co",
-      category: "Food",
-      amount: -10.55,
-    },
-    {
-      date: "2019-12-02",
-      description: "South by Southwest Quinoa Bowl at Fresh & Co",
-      category: "Food",
-      amount: -10.55,
-    },
-    {
-      date: "2019-12-04",
-      description: "Sunglasses, Urban Outfitters",
-      category: "Fashion",
-      amount: -24.99,
-    },
-    {
-      date: "2019-12-06",
-      description: "Venmo, Alice Pays you for Burrito",
-      category: "Food",
-      amount: 8.75,
-    },
-    {
-      date: "2019-12-06",
-      description: "Chipotle",
-      category: "Food",
-      amount: -17.59,
-    },
-  ]);
-
-  const addNewTransaction = (newTransaction) => {
-    setTransactions([...transactions, newTransaction]);
-  };
-
-  const searchNew = (searchQuery) => {
-    const filteredTransactions = transactions.filter((transaction) => {
-      return transaction.description
-        .toLowerCase()
-        .includes(searchQuery.toLowerCase());
-    });
-
-    setTransactions(filteredTransactions);
-  };
   return (
     <div className="App">
       <Header />
-      <SearchBox onSearch={searchNew} />
-      <AddTransactions onSubmit={addNewTransaction} />
-      <TableDataList transactions={transactions} />
+      <SearchBox />
     </div>
   );
 }
--- bank-of-flatiron/src/components/Search.js
+++ bank-of-flatiron/src/components/Search.js
@@ -1,1 +1,23 @@
+import { useState } from "react";
 
+const SearchBox = ({ onSearch }) => {
+  const [query, setQuery] = useState("");
+
+  const handleSearch = (event) => {
+    setQuery(event.target.value);
+    onSearch(event.target.value);
+  };
+
+  return (
+    <div className="search">
+      <input
+        type="text"
+        placeholder="Search your Recent Transactions"
+        value={query}
+        onChange={handleSearch}
+      />
+    </div>
+  );
+};
+
+export default SearchBox;
