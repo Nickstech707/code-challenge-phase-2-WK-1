8c4b3ffabf724c99543b226cebcd6255b87a91c0
Nicholas Muriithi <nickstech707@gmail.com>
2024-04-29 12:31:19 +0300

    final commit

--- bank-of-flatiron/src/components/AddTransactions.js
+++ bank-of-flatiron/src/components/AddTransactions.js
@@ -1,1 +1,71 @@
+import { useState } from "react";
 
+const AddTransactionForm = ({ onSubmit }) => {
+  const [date, setDate] = useState("");
+  const [amount, setAmount] = useState("");
+  const [description, setDescription] = useState("");
+  const [category, setCategory] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const newTransaction = {
+      id: Date.now(),
+      date,
+      amount: parseFloat(amount) || 0,
+      description,
+      category,
+    };
+    onSubmit(newTransaction);
+    setDate("");
+    setAmount("");
+    setDescription("");
+    setCategory("");
+  };
+
+  return (
+    <>
+      <form className="form" onSubmit={handleSubmit}>
+        <div id="form-inputs">
+          <label
+            style={{ display: "flex", marginTop: "10px", fontWeight: "bold" }}
+          >
+            Date:
+          </label>
+          <input
+            className="text-input"
+            type="date"
+            value={date}
+            onChange={(event) => setDate(event.target.value)}
+          />
+
+          <input
+            className="text-input"
+            type="text"
+            placeholder="Description"
+            value={description}
+            onChange={(event) => setDescription(event.target.value)}
+          />
+
+          <input
+            className="text-input"
+            type="text"
+            placeholder="Category"
+            value={category}
+            onChange={(event) => setCategory(event.target.value)}
+          />
+
+          <input
+            className="text-input"
+            type="number"
+            placeholder="Amount"
+            value={amount}
+            onChange={(event) => setAmount(event.target.value)}
+          />
+        </div>
+        <button type="submit">Add Transaction</button>
+      </form>
+    </>
+  );
+};
+
+export default AddTransactionForm;
--- bank-of-flatiron/src/components/Search.js
+++ bank-of-flatiron/src/components/Search.js
@@ -1,21 +1,30 @@
 import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 const SearchBox = ({ onSearch }) => {
   const [query, setQuery] = useState("");
-
   const handleSearch = (event) => {
-    setQuery(event.target.value);
-    onSearch(event.target.value);
+    const searchText = event.target.value;
+    setQuery(searchText);
+
+    if (searchText === "") {
+      window.location.reload();
+    } else {
+      onSearch(searchText);
+    }
   };
 
   return (
-    <div className="search">
+    <div className="search-bar">
       <input
+        id="search"
         type="text"
         placeholder="Search your Recent Transactions"
         value={query}
         onChange={handleSearch}
       />
+      <FontAwesomeIcon icon={faSearch} className="search-icon" />
     </div>
   );
 };
